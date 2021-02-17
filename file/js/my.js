5b84a5403bbf1d2f4e818a97a4efbe0973f83e16
vscc1211 <63036155+vscc1211@users.noreply.github.com>
2021-02-17 19:02:36 +0800

    Add files via upload

---
+++ file/js/my.js
@@ -0,0 +1,24 @@
+$('#myTb').DataTable({
+language: {
+"emptyTable": "無資料...",
+"processing": "處理中...",
+"loadingRecords": "載入中...",
+"lengthMenu": "每頁 _MENU_ 筆資料",
+"zeroRecords": "無搜尋結果",
+"info": "_START_ 至 _END_ / 共 _TOTAL_ 筆",
+"infoEmpty": "尚無資料",
+"infoFiltered": "(從 _MAX_ 筆資料過濾)",
+"infoPostFix": "",
+"search": "搜尋字串:",
+"paginate": {
+"first": "首頁",
+"last": "末頁",
+"next": "下頁",
+"previous": "前頁"
+},
+"aria": {
+"sortAscending": ": 升冪",
+"sortDescending": ": 降冪"
+}
+}
+});
